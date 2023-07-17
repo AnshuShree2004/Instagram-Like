1f3bc1453b10c0884fc1d01f176d47643b258fed
Anshu Shree <121965174+AnshuShree2004@users.noreply.github.com>
2023-07-17 21:13:49 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+
+const app = require('./app.js')
+const connectToDB = require('./confiq/db.confiq.js')
+const PORT = process.env.PORT || 3002
+
+
+app.listen(PORT, () => {
+    connectToDB()
+    console.log(`Server is running at http:// localhost: ${PORT}`)
+})
